1c289562c13b2a0df736364ef6a2a490672410d7
Made I.T <info@madeit.be>
2020-06-26 20:58:54 +0200

    Update install.sh

--- composer/install/install.sh
+++ composer/install/install.sh
@@ -27,21 +27,11 @@ check_args '0' "$#" '[RESTART]'
 #                       Action                             #
 #----------------------------------------------------------#
 
-EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
-
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
-fi
-
-php composer-setup.php --quiet
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 mv composer.phar /usr/bin/composer
 RESULT=$?
-rm composer-setup.php
+php -r "unlink('composer-setup.php');"
 
 $BIN/v-add-cron-job admin '27' '2' '1' '*' '*' "$VESTA/plugin/composer/bin/v-update-composer"
 
